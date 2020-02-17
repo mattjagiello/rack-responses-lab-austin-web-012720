0727628a92ac797ef0cb1e307e655d4ce7df493e
Matthew Jagiello <mjagiello@gmail.com>
2020-02-17 18:50:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
+  def pretty_response
+    (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
   end
 
 end
